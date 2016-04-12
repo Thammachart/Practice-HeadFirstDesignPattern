569c9df0b3fd219e9c555f2157e57255fa0be934
Thammachart <bustheheroall@gmail.com>
2016-04-12 19:01:20 +0700

    WeatherStation 1st draft complete

---
+++ 2-TheObserverPattern/1-WeatherMonitoring/src/main/java/org/weather/display/CurrentConditionsDisplay.java
@@ -0,0 +1,27 @@
+package org.weather.display;
+
+import org.weather.Observer;
+import org.weather.Subject;
+
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+    private float temperature;
+    private float humidity;
+    private Subject weatherData;
+
+    public CurrentConditionsDisplay(Subject weatherData) {
+        this.weatherData = weatherData;
+        this.weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void update(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        display();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Current conditions: " + temperature + "F degrees and " + humidity + "% humidity");
+    }
+}
