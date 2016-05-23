210bcdd449dc4227c2ac0e020cc227daa4325a26
Thammachart <bustheheroall@gmail.com>
2016-05-23 17:17:51 +0700

    Template Method Pattern of StarBuzz roughly completed

---
+++ 8-TemplateMethod/1-StarBuzz/src/main/java/org/starbuzz/beverage/CaffeineBeverage.java
@@ -0,0 +1,21 @@
+package org.starbuzz.beverage;
+
+public abstract class CaffeineBeverage {
+    public final void prepareRecipe() {
+        boilWater();
+        brew();
+        pourInCup();
+        addCondiments();
+    }
+
+    abstract void brew();
+    abstract void addCondiments();
+
+    void boilWater() {
+        System.out.println("Boiling Water");
+    }
+
+    void pourInCup() {
+        System.out.println("Pouring into cup");
+    }
+}
---
+++ 8-TemplateMethod/1-StarBuzz/src/main/java/org/starbuzz/beverage/Coffee.java
@@ -0,0 +1,13 @@
+package org.starbuzz.beverage;
+
+public class Coffee extends CaffeineBeverage {
+    @Override
+    void brew() {
+        System.out.println("Dripping Coffee through filter");
+    }
+
+    @Override
+    void addCondiments() {
+        System.out.println("Adding Sugar and Milk");
+    }
+}
